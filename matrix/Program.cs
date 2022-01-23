d7f6e8236e01d89acf9ac7d4f35c3dd442f10616
hummusbird <hummusbirb@gmail.com>
2022-01-23 14:58:20 +0000

    feat: -c -l -P -ascii

--- matrix/Program.cs
+++ matrix/Program.cs
@@ -9,32 +9,92 @@ using System.Threading.Tasks;
 
 namespace matrix
 {
-
     class Program
     {
-        public static void ThreadLine()
-        {
-            Line line = new Line(Program.rnd.Next(0, Console.BufferWidth - 1));
-            while (true)
-            {
-                line.DrawLine();
-                Thread.Sleep(50);
-            }
-        }
-
+        public static bool perfmode = false;
+        public static ConsoleColor fg = ConsoleColor.DarkGreen;
+        public static ConsoleColor bg = ConsoleColor.Green;
         public static Random rnd = new Random();
+        public static int asc1 = 32, asc2 = 256;
+
+        public static int len1 = 5, len2 = 25;
 
         static void Main(string[] args)
         {
+            foreach(string arg in args)
+            {
+                int pos = Array.FindIndex(args, v => v == arg);
+
+                switch (arg)
+                {
+                    case "-c": // colour
+                        if (args.Length >= pos + 2)
+                        {
+                            switch (args[pos + 1].ToLower())
+                            {
+                                case "red":
+                                    fg = ConsoleColor.Red;
+                                    bg = ConsoleColor.DarkRed;
+                                    break;
+                                case "blue":
+                                    fg = ConsoleColor.Blue;
+                                    bg = ConsoleColor.DarkBlue;
+                                    break;
+                                case "purple":
+                                    fg = ConsoleColor.Magenta;
+                                    bg = ConsoleColor.DarkMagenta;
+                                    break;
+                                case "gray":
+                                    fg = ConsoleColor.Gray;
+                                    bg = ConsoleColor.DarkGray;
+                                    break;
+                                case "cyan":
+                                    fg = ConsoleColor.Cyan;
+                                    bg = ConsoleColor.DarkCyan;
+                                    break;
+                                case "yellow":
+                                    fg = ConsoleColor.Yellow;
+                                    bg = ConsoleColor.DarkYellow;
+                                    break;
+                            }
+                        }
+                        break;
+                    case "-P": // performance mode (doesn't randomise chars or draw fadeout)
+                        perfmode = true;
+                        break;
+                    case "-ascii": // ascii charset
+                        if (args.Length >= pos + 2)
+                        {
+                            try
+                            {
+                                Program.asc1 = int.Parse(args[pos + 1].Split(',')[0]);
+                                Program.asc2 = int.Parse(args[pos + 1].Split(',')[1]);
+                            }
+                            catch { }
+                        }
+                        break;
+                    case "-l": // rain length
+                        if (args.Length >= pos + 2)
+                        {
+                            try
+                            {
+                                Program.len1 = int.Parse(args[pos + 1].Split(',')[0]);
+                                Program.len2 = int.Parse(args[pos + 1].Split(',')[0]);
+                                Program.len2 = int.Parse(args[pos + 1].Split(',')[1]);
+                            }
+                            catch { }
+                        }
+                        break;
+                }
+            }
+
             Console.CursorVisible = false;
 
             List<Line> lines = new List<Line>();
 
             for (int i = 0; i < Console.BufferWidth -1; i++)
             {
-                //Thread t = new Thread(new ThreadStart(ThreadLine));
-                //t.Start();
-                Line line = new Line(i); //rnd.Next(0, Console.BufferWidth - 1));
+                Line line = new Line(i);
                 lines.Add(line);
             }
 
@@ -74,7 +134,7 @@ namespace matrix
     public class Line
     {
         static int delay = 100;
-        int loop = Program.rnd.Next(5, 25);
+        int loop = Program.rnd.Next(Program.len1, Program.len2);
         int row;
         int column = 0 - Program.rnd.Next(delay);
         List<char> charlist = new List<char>();
@@ -85,7 +145,7 @@ namespace matrix
             
             for (int i = 0; i < 250; i++)
             {
-                charlist.Add(Convert.ToChar(Program.rnd.Next(32,256)));
+                charlist.Add(Convert.ToChar(Program.rnd.Next(Program.asc1,Program.asc2)));
             }
         }
 
@@ -93,22 +153,25 @@ namespace matrix
         {
             for (int i = 0; i < (loop < column ? loop : column); i++)
             {
-                charlist[Program.rnd.Next(0, 250)] = Convert.ToChar(Program.rnd.Next(32,256));
+                if (!Program.perfmode) { charlist[Program.rnd.Next(0, 250)] = Convert.ToChar(Program.rnd.Next(Program.asc1, Program.asc2)); }
 
                 if (column - i < (Window.GetPosition().Bottom - Window.GetPosition().Top - 39) / 16)
                 {
-                    Console.SetCursorPosition(row, column - i - 1);
+                    bool run = !Program.perfmode;
 
-                    bool run = true;
-
-                    ConsoleColor colour = ConsoleColor.DarkGreen;
+                    ConsoleColor colour = Program.fg;
                     if (i == loop - 1) { colour = ConsoleColor.Black; run = true; }
-                    else if (i == loop - 2) { colour = ConsoleColor.Green; run = true; }
+                    else if (i == loop - 2) { colour = Program.bg; }
                     else if (i == 1) { run = true; }
                     else if (i == 0) { colour = ConsoleColor.White; run = true; }
 
-                    Console.ForegroundColor = colour;
-                    if (run) { Console.WriteLine(charlist[column - i]); }
+
+                    if (run) 
+                    {
+                        Console.ForegroundColor = colour;
+                        Console.SetCursorPosition(row, column - i - 1); 
+                        Console.WriteLine(charlist[column - i]); 
+                    }
                 }
             }
             
@@ -117,7 +180,7 @@ namespace matrix
             if (column - loop > (Window.GetPosition().Bottom - Window.GetPosition().Top - 39) / 16)
             {
                 column = 0 - Program.rnd.Next(delay);
-                loop = Program.rnd.Next(5, 25);
+                loop = Program.rnd.Next(Program.len1, Program.len2);
             }
         }
     }
